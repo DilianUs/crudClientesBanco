704a33e4ccd7b483ad1bc2bd215e5047476acf8e
DilianUs <75262812+DilianUs@users.noreply.github.com>
2023-01-27 17:22:16 +0000

    upload de archivos iniciales del crud

---
+++ LectorArchivos.java
@@ -0,0 +1,25 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+ * Author:Dilian Us
+ * date:16/01/2023
+ * email:dilian0217@gmail.com
+ */
+public class LectorArchivos {
+    public void leerArchivo(String direccionArchivo,ArrayList<String> clientes) {
+        try{
+            Scanner scanner = new Scanner(Paths.get(direccionArchivo));
+            String lineaTexto;
+            while(scanner.hasNextLine()){
+                lineaTexto = scanner.nextLine();
+                clientes.add(lineaTexto);
+            } 
+       
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+            
+        }  
+    }
+}
